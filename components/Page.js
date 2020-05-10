46a6edc41995d14ae46334f70d4cce2bf5f766b1
AmoDinho <u14284783@tuks.co.za>
2020-05-10 21:05:28 +0200

    creating a page component

---
+++ components/Page.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Page = ({ props }) => {
+  <>{props.children}</>;
+};
+
+export async function getStaticProps() {
+  let props = {};
+  if (Component.getInitalProps) {
+    props = await Component.getInitalProps(ctx);
+  }
+
+  // By returning { props: posts }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props,
+  };
+}
+
+export default Page;
